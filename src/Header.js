938207797a8bf65ad532ba74e1bc73733a579f7d
Marco Cedaro <cedmax@users.noreply.github.com>
2017-10-27 17:02:42 +0100

    added a spacer component

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import cssStyles from './Header.module.css'
 import Search from './Search'
+import Spacer from './Spacer'
 
 export default class Graph extends Component {
   render () {
@@ -20,22 +21,22 @@ export default class Graph extends Component {
         <nav className={cssStyles.nav}>
           <small className={cssStyles.line}>
             Sort by:
-            {' '}<a className={!sorted ? cssStyles.selected : ''} onClick={() => sortBy()}>default</a>
-            {' '}<a className={sorted === 'az' ? cssStyles.selected : ''} onClick={() => sortBy('az')}>a-z</a>            
-            {' '}<a className={sorted === 'rating' ? cssStyles.selected : ''} onClick={() => sortBy('rating')}>avg. rating</a>
-            {' '}<a className={sorted === 'year' ? cssStyles.selected : ''} onClick={() => sortBy('year')}>least recent</a>
-            {' '}<a className={sorted === 'year_desc' ? cssStyles.selected : ''} onClick={() => sortBy('year_desc')}>most recent</a>
+            <Spacer/><a className={!sorted ? cssStyles.selected : ''} onClick={() => sortBy()}>default</a>
+            <Spacer/><a className={sorted === 'az' ? cssStyles.selected : ''} onClick={() => sortBy('az')}>a-z</a>            
+            <Spacer/><a className={sorted === 'rating' ? cssStyles.selected : ''} onClick={() => sortBy('rating')}>avg. rating</a>
+            <Spacer/><a className={sorted === 'year' ? cssStyles.selected : ''} onClick={() => sortBy('year')}>least recent</a>
+            <Spacer/><a className={sorted === 'year_desc' ? cssStyles.selected : ''} onClick={() => sortBy('year_desc')}>most recent</a>
           </small>
           <div className={`${cssStyles.line} ${cssStyles.distribute}`}>
             <small>
               Tooltips:
-              {' '}<a className={!visible ? cssStyles.selected : ''} onClick={() => show()}>posters</a>
-              {' '}<a className={visible === 'title' ? cssStyles.selected : ''} onClick={() => show('title')}>titles</a>
+              <Spacer/><a className={!visible ? cssStyles.selected : ''} onClick={() => show()}>posters</a>
+              <Spacer/><a className={visible === 'title' ? cssStyles.selected : ''} onClick={() => show('title')}>titles</a>
             </small>
             <small>
             View:
-              {' '}<a className={trilogies ? cssStyles.selected : ''} onClick={() => toggleTrilogies()}>trilogies</a>
-              {' '}<a className={!trilogies ? cssStyles.selected : ''} onClick={() => toggleTrilogies()}>all</a>
+              <Spacer/><a className={trilogies ? cssStyles.selected : ''} onClick={() => toggleTrilogies()}>trilogies</a>
+              <Spacer/><a className={!trilogies ? cssStyles.selected : ''} onClick={() => toggleTrilogies()}>all</a>
             </small>
           </div>
         </nav>
