37597ce424d9082926a38c0b2eb95abd4e49f33e
Marco Cedaro <cedmax@users.noreply.github.com>
2018-01-20 14:42:04 +0000

    removed unused import

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Nav from './Navigation'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import cssStyles from './Header.module.css'
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Button from './Button'
 import cssStyles from './Navigation.module.css'
 
