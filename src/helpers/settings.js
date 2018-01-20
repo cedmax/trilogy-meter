3377cf13f76c0492a3eadd6afcae680ccdfa060c
Marco Cedaro <cedmax@users.noreply.github.com>
2018-01-20 15:00:59 +0000

    cloned array before sorting

--- src/helpers/settings.js
+++ src/helpers/settings.js
@@ -2,24 +2,22 @@ import {
   camelCasify
 } from './utils'
 
-export const filter = (series, search) => {
-  return search ? series.filter((series) => {
-    return series.title.toLowerCase().indexOf(search.toLowerCase()) > -1
-  }) : series
-}
+export const filter = (series, search) => series.filter((series) => (
+  series.title.toLowerCase().indexOf(search.toLowerCase()) > -1
+))
 
 export const sorting = {
-  az: (series) => series.sort((a, b) => {
+  az: (series) => [...series].sort((a, b) => {
     if (a.title < b.title) return -1
     if (a.title > b.title) return 1
     return 0
   }),
-  year: (series) => series.sort((a, b) => b.movies[0].year - a.movies[0].year),
+  year: (series) => [...series].sort((a, b) => b.movies[0].year - a.movies[0].year),
   rating: (series, trilogies) => {
     const average = camelCasify(trilogies ? 'trilogy' : '', 'average')
     const range = camelCasify(trilogies ? 'trilogy' : '', 'range')
 
-    return series.sort((a, b) => (
+    return [...series].sort((a, b) => (
       b[average] !== a[average]
         ? b[average] - a[average]
         : a[range] - b[range]
@@ -29,7 +27,7 @@ export const sorting = {
     const average = camelCasify(trilogies ? 'trilogy' : '', 'average')
     const range = camelCasify(trilogies ? 'trilogy' : '', 'range')
 
-    return series.sort((a, b) => (
+    return [...series].sort((a, b) => (
       b[range] !== a[range]
         ? a[range] - b[range]
         : b[average] - a[average]
