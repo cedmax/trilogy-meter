064a1c569bd7d39be94619b83f8f684bd4bd5384
Marco Cedaro <cedmax@users.noreply.github.com>
2019-01-09 19:58:14 +0000

    fixed timestamp

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import React from 'react'
-import App from './App'
-import {
-  render,
-  hydrate
-} from 'react-dom'
-import registerServiceWorker from './registerServiceWorker'
-import './index.css'
+import React from "react";
+import App from "./App";
+import { render, hydrate } from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 
-const dataNode = document.getElementById('movies')
-const updatedAt = parseInt(dataNode.getAttribute('data-updated'), 10)
-const data = JSON.parse(dataNode.innerHTML)
-const rootNode = document.getElementById('root')
+const dataNode = document.getElementById("movies");
+const { series, updated } = JSON.parse(dataNode.innerHTML);
+const rootNode = document.getElementById("root");
 
 if (rootNode.hasChildNodes()) {
-  hydrate(<App series={data} updatedAt={updatedAt} />, rootNode)
+  hydrate(<App series={series} updatedAt={updated} />, rootNode);
 } else {
-  render(<App series={data} updatedAt={updatedAt} />, rootNode)
+  render(<App series={series} updatedAt={updated} />, rootNode);
 }
 
-registerServiceWorker()
+registerServiceWorker();
