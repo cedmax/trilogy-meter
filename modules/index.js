0ae648ab49ef14bde2559c890b18ca206af3054c
Marco Cedaro <cedmax@users.noreply.github.com>
2021-09-13 00:05:24 +0100

    🙈

--- modules/index.js
+++ modules/index.js
@@ -1,3 +1,5 @@
-require('./fetch-firebase-data')()
-require('./fetch-rating')()
-require('./fetch-images')()
+;(async () => {
+  await require('./fetch-firebase-data')()
+  await require('./fetch-rating')()
+  await require('./fetch-images')()
+})()
