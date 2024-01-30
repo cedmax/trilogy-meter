979d0299ac4ccf523770e939c3b02475a8ee5e10
Marco Cedaro <cedmax@users.noreply.github.com>
2024-01-30 17:05:36 +0100

    cleaned up page load from image urls

--- modules/utils/saveData.js
+++ modules/utils/saveData.js
@@ -1,11 +1,26 @@
 const fs = require('fs')
 const cheerio = require('cheerio')
 
+const cleanData = (jsonData) => {
+  const newData = JSON.parse(jsonData);
+  for (const serie in newData.series) {
+    newData.series[serie].movies = newData.series[serie].movies.map(movie => {
+      delete movie.poster
+      return movie
+    })
+  }
+  return JSON.stringify(newData)
+}
+
+
 module.exports = data => {
   data.updated = Date.now()
   const jsonData = JSON.stringify(data)
   const $ = cheerio.load(fs.readFileSync(`./public/index.html`))
-  $('#movies').text(jsonData)
-  fs.writeFileSync('./public/index.html', $.html(), 'utf8')
   fs.writeFileSync('./modules/.tmp/data.json', jsonData, 'utf8')
+  
+  const newData = cleanData(jsonData)
+
+  $('#movies').text(newData)
+  fs.writeFileSync('./public/index.html', $.html(), 'utf8')
 }
