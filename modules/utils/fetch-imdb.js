5041fe2f36568c18c51d52e17fd45cd0a8f2d6b4
Marco Cedaro <cedmax@users.noreply.github.com>
2024-01-30 17:05:09 +0100

    changed source of data

--- modules/utils/fetch-imdb.js
+++ modules/utils/fetch-imdb.js
@@ -1,24 +1,22 @@
 const get = require('./cached-get')
 const cheerio = require('cheerio')
+const omdbkey = process.env.IMDB
 
 const fetchImdb = async movie => {
   try {
-    const data  = await get(
-      `https://www.imdb.com/title/${movie.imdb}/`
-    )
-    const $ = cheerio.load(data)
-    const img = $('.ipc-poster img').attr('src')
-    const schema = JSON.parse($('script[type="application/ld+json"]').html())
-    const rating = parseFloat(
-      schema.aggregateRating && schema.aggregateRating.ratingValue
-    )
-    const votes = parseInt(
-      schema.aggregateRating && schema.aggregateRating.ratingCount,
-      10
+    const data = await get(
+      `http://www.omdbapi.com/?apikey=${omdbkey}&i=${movie.imdb}`
     )
 
+    const rating = parseFloat(
+      data.imdbRating
+    )
+    const votes = parseInt(
+      data.imdbVotes.replace(/,/g, '')
+    , 10)
+
     return {
-      poster: img,
+      poster: data.Poster,
       votes,
       rating: isNaN(rating) ? null : rating
     }
